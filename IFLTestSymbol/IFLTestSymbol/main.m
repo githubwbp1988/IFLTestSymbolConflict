bb0665c8ece125a36701100c1e582d93736d5377
githubwbp1988 <wbp123@yeah.net>
2022-07-23 01:35:35 +0800

    pending ..

--- IFLTestSymbol/IFLTestSymbol/main.m
+++ IFLTestSymbol/IFLTestSymbol/main.m
@@ -32,6 +32,7 @@ int main(int argc, const char * argv[]) {
     libIFLTestStaticLib_global_func();
     
     // OTHER_LDFLAGS = $(inherited) -Xlinker -U -Xlinker _mm_func 骗过链接器ld 告诉链接器这个符号不需要管有没有定义
+    // 虽然骗过了链接器，但是骗不过动态链接器 动态链接器扫描到这儿的时候，就去找mm_func，发现只有名称，没有函数体 Symbol not found: _mm_func
 //    mm_func();
     
     
